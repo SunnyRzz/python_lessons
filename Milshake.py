ebb237422c60e2852a6f762483c9659386724970
SunnyRzz <sunny.rai@equalexperts.com>
2024-01-17 11:28:01 +0000

    wednesday morning commit

---
+++ Milshake.py
@@ -0,0 +1,13 @@
+budget = float(input("Please enter your budget: "))
+milkshakes = {1: ("Chocolate",2.00), 2: ("Vanilla",3.00), 3: ("Banana",4.00), 4: ("Strawberry",5.00)}
+
+
+print("""Milkshake Menu!: 
+\n 1.	Chocolate
+\n 2.	Vanilla
+\n 3.	Banana
+\n 4.	Strawberry
+""")
+
+for flavour, price in milkshakes.items():
+    print(f"{flavour} costs {price}")
---
+++ Wednesday challenge.py
@@ -0,0 +1,11 @@
+num = 10
+num_1=1
+
+while num > num_1:
+    print(num)
+    break
+else:
+    print(num_1)
+
+largest = num *(num> num_1) + num_1 * (num_1 > num)
+# 

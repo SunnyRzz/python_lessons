388319bb901437319235454eaaaba455875976a4
SunnyRzz <sunny.rai@equalexperts.com>
2024-01-18 15:56:31 +0000

    Thursday Labs

--- Lab classes.py
+++ Lab classes.py
@@ -18,27 +18,28 @@ print(student2)
 
 #Part2
 class Student:
-    def __init__(self, name, age, _class, test1, test2, test3):
+    def __init__(self, name, age, _class):
         self.name = name
         self.age = age
         self._class= _class
-        self.test1 = test1
-        self.test2 = test2
-        self.test3 = test3
+
 
     def how_old(self):
         print(f"{self.name} is {self.age} years old")
 
-    def average_scores(self):
-        print(f"{self.name}'s score average is: ", (self.test1 + self.test2 +self.test3)/3)
+    def average_scores(self, test1, test2, test3):
+        self.test1 = test1
+        self.test2 = test2
+        self.test3 = test3
+        print(f"{self.name}'s score average is: ", round((self.test1 + self.test2 +self.test3)/3))
 
     def __str__(self):
-        return f"{self.name}, {self.age}, {self._class}, {self.test1}, {self.test2}, {self.test3}"
+        return f"{self.name}, {self.age}, {self._class}"
     
-student1=Student("Adam", 18, "Junior", 70, 80, 75)
-student2=Student("John", 21, "Senior", 90, 85, 80)
+student1=Student("Adam", 18, "Junior")
+student2=Student("John", 21, "Senior")
 
-student1.average_scores()
+student1.average_scores(75,35,50)
 
 print(student2)
 
@@ -72,6 +73,9 @@ class Dodo(Bird):
     def details(self):
         print(f"The bird breed {self.breed} is {self.extinct}")
 
+    def deet(self):
+        return super().__str__() + f"exitint - {self.extinct}"
+
 pigeon=Bird("Pidgy", "Pigeon")
 print(pigeon)
 
@@ -81,4 +85,5 @@ print(owl)
 
 dodo=Dodo("Doddy", "Dodo", "Extinct")
 dodo.details()
+print(dodo.deet())
 print(dodo)

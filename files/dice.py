a6ce0f18a49b8aa660967305f5005d442758fa84
SunnyRzz <sunny.rai@equalexperts.com>
2024-01-19 10:10:09 +0000

    Friday Labs

---
+++ files/dice.py
@@ -0,0 +1,5 @@
+from random import randint
+
+def diceroll():
+    result = randint(1,6)
+    return result
---
+++ files/dice_game.py
@@ -0,0 +1,5 @@
+import dice
+
+print("The first Dice you rolled was",dice.diceroll())
+print("The second Dice you rolled was",dice.diceroll())
+
